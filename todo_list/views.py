afa80cc240ad20dedd65678ebcaf1a200de04628
pashaetesam <pauletesam@comcast.net>
2020-03-20 12:36:28 -0700

    fassfas

---
+++ todo_list/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'todo_list/home.html',{})
+
+def about(request):
+    return render(request,'todo_list/about.html',{})
+
